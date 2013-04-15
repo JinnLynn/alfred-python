5306e3c2f5a3d600b677b235cc632cdc1d0b7f25
JinnLynn <eatfishlin@gmail.com>
2013-04-15 11:53:31 +0800

    cache不使用类，直接提供接口

--- alfred/__init__.py
+++ alfred/__init__.py
@@ -8,7 +8,7 @@
 
 from .core import *
 
-from .cache import Cache
+import cache
 
 from .config import Config
 
--- alfred/cache.py
+++ alfred/cache.py
@@ -7,73 +7,60 @@ from core import __cache_folder__
 
 # { 'expire_time' : 0, data' : {} }
 
-CACHE_DEFAULT_EXPIRE = 60 * 60 * 24
+_DEFAULT_EXPIRE = 60 * 60 * 24
 
-class Cache(object):
-    def __init__(self):
-        self.cache_dir = os.path.join(__cache_folder__, core.bundleID())
-        if not os.path.exists(self.cache_dir):
-            os.makedirs(self.cache_dir)
+def _getFilepath(name):
+    cache_dir = os.path.join(__cache_folder__, core.bundleID())
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+    # convert to md5, more safe for file name
+    name = hashlib.md5(name).hexdigest()
+    return os.path.join(self.cache_dir, '{}.json'.format(name))
 
-    def getFilepath(self, name):
-        # convert to md5, more safe for file name
-        name = hashlib.md5(name).hexdigest()
-        return os.path.join(self.cache_dir, '{}.json'.format(name))
+def _getContent(name):
+    try:
+        filepath = _getFilepath(name)
+        with codecs.open(path, 'r', 'utf-8') as f:
+            return json.load(f)
+    except:
+        pass
 
-    def getContent(self, name):
-        try:
-            path = self.getFilepath(name)
-            with codecs.open(path, 'r', 'utf-8') as f:
-                return json.load(f)
-        except:
-            pass
-    
-    def get(self, name):
-        try:
-            cache = self.getContent(name)
-            if cache['expire_time'] >= time.time():
-                return cache['data']
-        except:
-            pass
-        self.delete(name)
-        
-    def set(self, name, data, expire=CACHE_DEFAULT_EXPIRE):
-        path = self.getFilepath(name)
-        try:
-            cache = {
-                    'expire_time'   : time.time() + expire,
-                    'data'          : data
-                }
-            with codecs.open(path, 'w', 'utf-8') as f:
-                json.dump(cache, f, indent=4)
-        except:
-            pass
-
-    def delete(self, name):
-        path = self.getFilepath(name)
-        if os.path.exists(path):
-            os.remove(path)
-
-    def clean(self):
-        shutil.rmtree(self.cache_dir)
-
-    def expireTimeout(self, name):
-        try:
-            cache = self.getContent(name)
-            if cache['expire_time'] >= time.time():
-                return cache['expire_time'] - time.time()
-        except:
-            pass
-        return -1
-
-def set(name, data, expire=CACHE_DEFAULT_EXPIRE):
-    Cache().set(name, data, expire)
+def set(name, data, expire=_DEFAULT_EXPIRE):
+    filepath = _getFilepath(name)
+    try:
+        cache = {
+            'expire_time'   : time.time() + expire,
+            'data'          : data
+            }
+        with codecs.open(filepath, 'w', 'utf-8') as f:
+            json.dump(cache, f, indent=4)
+    except:
+        pass
 
 def get(name):
-    return Cache().get(name)
+    try:
+        cache = _getContent(name)
+        if cache['expire_time'] >= time.time():
+            return cache['data']
+    except:
+        pass
+    delete(name)
 
 def delete(name):
-    Cache().delete(name)
+    cache_file = _getFilepath(name)
+    if os.path.exists(cache_file):
+        os.remove(cache_file)
 
 def clean():
-    Cache().clean()
+    cache_dir = os.path.join(__cache_folder__, core.bundleID())
+    if os.path.exists(cache_dir):
+        shutil.rmtree(cache_dir)
+
+def expireTimeout(self, name):
+    try:
+        cache = _getContent(name)
+        if cache['expire_time'] >= time.time():
+            return cache['expire_time'] - time.time()
+    except:
+        pass
+    return -1
