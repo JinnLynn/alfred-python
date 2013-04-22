b1e980a4f654b4e59cd1f6634d846a06f6569009
JinnLynn <eatfishlin@gmail.com>
2013-04-22 14:02:08 +0800

    uid not base on bundle id

--- alfred/util.py
+++ alfred/util.py
@@ -6,4 +6,4 @@ import core
 
 hashDigest = lambda s: hashlib.md5(s).hexdigest()
 
-uid = lambda: '{0}.{1}'.format(core.bundleID(), random.getrandbits(25))
+uid = lambda: hashDigest('{}'.format(random.getrandbits(25)))
